9dcb9bca15bd2a66e841ed24492b2a4ffba0d9ac
unknown <selvapraveen@bitsathy.ac.in>
2024-04-24 20:37:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TodoList = () => {
+    const [taskInput, setTaskInput] = useState('');
+    const [tasks, setTasks] = useState([]);
 
-export default App;
+    const addTask = () => {
+        if (taskInput.trim() !== '') {
+            setTasks([...tasks, taskInput]);
+            setTaskInput('');
+        } else {
+            alert('Enter The Task');
+        }
+    };
+
+    const deleteTask = index => {
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className="container">
+            <h1>To-Do List</h1>
+            <input
+                type="text"
+                value={taskInput}
+                onChange={e => setTaskInput(e.target.value)}
+                placeholder="Enter Task..."
+            />
+            <button className='buttonn' onClick={addTask}>Add Task</button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <span className="task">{task}</span>
+                        <button className="delete-btn" onClick={() => deleteTask(index)}>X</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
